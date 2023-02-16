447b8d7806b2bd7597595718e80bdd8ceb10f684
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2023-02-16 15:34:30 -0300

    added sentiment analysis model using TextBlob & new endpoint

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from textblob import TextBlob
 
-app = Flask('meu_app')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Minha primeira API em python"
 
-app.run()
+@app.route('/sentimento/<frase>')
+def sentimento(frase):
+    tb = TextBlob(frase)
+    tb_en = tb.translate(from_lang='pt_br', to='en')
+    polaridade = tb_en.sentiment.polarity
+    return f"Polaridade: {polaridade}"
+
+app.run(debug=True)
+
