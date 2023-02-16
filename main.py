c2fa6645f51cde3e57275fd248c30f34f83a2cf1
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2023-02-16 18:30:47 -0300

    added price analysis model using sklearn & new endpoint

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 from flask import Flask
 from textblob import TextBlob
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_csv('casas.csv')
+colunas = ['tamanho', 'preco']
+df = df[colunas]
+
+x = df.drop('preco', axis=1) #variavel explicativa
+y = df['preco'] #variavel resposta
+
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.3, random_state=42
+    )
+
+modelo = LinearRegression()
+modelo.fit(x_train,y_train)
 
 app = Flask(__name__)
 
@@ -13,6 +30,11 @@ def sentimento(frase):
     tb_en = tb.translate(from_lang='pt_br', to='en')
     polaridade = tb_en.sentiment.polarity
     return f"Polaridade: {polaridade}"
+
+@app.route('/cotacao/<int:tamanho>')
+def cotacao(tamanho):
+    preco = modelo.predict([[tamanho]])
+    return str(preco)
 
 app.run(debug=True)
 
