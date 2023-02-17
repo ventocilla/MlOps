3dcedb1e83746601110dc0661ef3fb41c672ffae
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2023-02-16 22:38:00 -0300

    added basic auth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_basicauth import BasicAuth
 from textblob import TextBlob
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -19,12 +20,17 @@ modelo = LinearRegression()
 modelo.fit(x_train,y_train)
 
 app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = 'julio'
+app.config['BASIC_AUTH_PASSWORD'] = 'alura'
+
+basic_auth = BasicAuth(app)
 
 @app.route('/')
 def home():
     return "Minha primeira API em python"
 
 @app.route('/sentimento/<frase>')
+@basic_auth.required
 def sentimento(frase):
     tb = TextBlob(frase)
     tb_en = tb.translate(from_lang='pt_br', to='en')
